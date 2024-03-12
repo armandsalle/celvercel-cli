e5512f0305001bb824e8c8bca604eda28c2cbfc6
armand <armand.salle@gmail.com>
2024-03-12 11:59:13 +0100

    Fix npx and bin issues

--- postinstall.js
+++ postinstall.js
@@ -16,7 +16,7 @@ var _typeof =
       }
 
 var path = require("path"),
-  mkdirp = require("mkdirp"),
+  { mkdirp } = require("mkdirp"),
   fs = require("fs")
 
 // Mapping from Node's `process.arch` to Golang's `$GOARCH`
@@ -36,26 +36,8 @@ var PLATFORM_MAPPING = {
 }
 
 async function getInstallationPath() {
-  // `npm bin` will output the path where binary files should be installed
-
-  const value = await execShellCommand("npm bin -g")
-
-  var dir = null
-  if (!value || value.length === 0) {
-    // We couldn't infer path from `npm bin`. Let's try to get it from
-    // Environment variables set by NPM when it runs.
-    // npm_config_prefix points to NPM's installation directory where `bin` folder is available
-    // Ex: /Users/foo/.nvm/versions/node/v4.3.0
-    var env = process.env
-    if (env && env.npm_config_prefix) {
-      dir = path.join(env.npm_config_prefix, "bin")
-    }
-  } else {
-    dir = value.trim()
-  }
-
-  await mkdirp(dir)
-  return dir
+  await mkdirp("bin")
+  return "bin"
 }
 
 async function verifyAndPlaceBinary(binName, binPath, callback) {
